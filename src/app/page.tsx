70a9c13732cf338bbe51728aea1fe295c1d6a7e8
sthallysson <sthallysson5@gmail.com>
2024-01-05 01:56:41 -0300

    feat: add put event in button editar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,16 @@ import { useEffect, useState } from 'react';
 
 import { SignOutButton } from '@/components/SignOutButton';
 import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 import { api } from '@/services/api';
 import { User } from '@/types/interfaces';
@@ -13,6 +23,8 @@ export default function Home() {
   const { data: session } = useSession();
 
   const [users, setUsers] = useState<User[]>([]);
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
 
   const getUsers = async () => {
     try {
@@ -37,6 +49,22 @@ export default function Home() {
     }
   }
 
+  async function handleEditUser(userId: string) {
+    users.map(async (user) => {
+      if (user.id === userId) {
+        try {
+          if (name === '' || email === '') throw Error;
+
+          await api.put(`/users/${userId}`, { ...user, name, email });
+        } catch (error) {
+          console.log('Error:', error);
+        }
+      }
+    });
+
+    getUsers();
+  }
+
   return (
     <div className="h-screen w-full bg-zinc-100 flex flex-col justify-center items-center gap-4">
       <h1 className="text-3xl font-bold">Bem vindo {session?.user?.name}.</h1>
@@ -55,26 +83,75 @@ export default function Home() {
           </tr>
         </thead>
         <tbody className="text-left border border-zinc-950 rounded-full ">
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td className="border border-zinc-950 px-4 py-2">{user.name}</td>
-              <td className="border border-zinc-950 px-4 py-2">{user.email}</td>
-              <td className="border border-zinc-950 px-4 py-2">
-                <Button className="bg-blue-700 text-white hover:bg-blue-600">
-                  Editar
-                </Button>
-              </td>
-              <td className="border border-zinc-950 px-4 py-2">
-                <Button
-                  className="bg-red-700 text-white hover:bg-red-600"
-                  onClick={() => handleDeleteUser(user.id)}
-                  disabled={session?.user?.email === user.email}
-                >
-                  Deletar
-                </Button>
-              </td>
-            </tr>
-          ))}
+          {users.map((user) => {
+            return (
+              <tr key={user.id}>
+                <td className="border border-zinc-950 px-4 py-2">
+                  {user.name}
+                </td>
+                <td className="border border-zinc-950 px-4 py-2">
+                  {user.email}
+                </td>
+                <td className="border border-zinc-950 px-4 py-2">
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Button className="bg-blue-700 text-white hover:bg-blue-600">
+                        Editar
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                      <DialogHeader>
+                        <DialogTitle>Editar Usuário</DialogTitle>
+                      </DialogHeader>
+                      <div className="grid gap-4 py-4">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                          <Label htmlFor="name" className="text-right">
+                            Nome
+                          </Label>
+                          <Input
+                            id="name"
+                            placeholder={user.name}
+                            className="col-span-3"
+                            onChange={(e) => setName(e.target.value)}
+                            value={name}
+                          />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                          <Label htmlFor="email" className="text-right">
+                            Email
+                          </Label>
+                          <Input
+                            id="email"
+                            placeholder={user.email}
+                            className="col-span-3"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                          />
+                        </div>
+                      </div>
+                      <DialogFooter>
+                        <Button
+                          type="submit"
+                          onClick={() => handleEditUser(user.id)}
+                        >
+                          Salvar Alterações
+                        </Button>
+                      </DialogFooter>
+                    </DialogContent>
+                  </Dialog>
+                </td>
+                <td className="border border-zinc-950 px-4 py-2">
+                  <Button
+                    className="bg-red-700 text-white hover:bg-red-600"
+                    onClick={() => handleDeleteUser(user.id)}
+                    disabled={session?.user?.email === user.email}
+                  >
+                    Deletar
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
       <SignOutButton />
